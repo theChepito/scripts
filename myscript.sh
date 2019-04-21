5810ff0a5e2aba74bf07175a3b3152b672b9218d
Joseph <joseph.p.schmitt@gmail.com>
2019-04-21 11:35:58 -0700

    Create basic script that prompts user for input

---
+++ myscript.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+#echo "What is your name?"
+#read yourname
+
+read -p "What is your name? " yourname
+
+echo "hello $yourname nice to meet you"
+
+echo "tell me the path of the file you want to open"
+
+read filepath
+
+open $filepath
+
+
+
+
+echo -------------------------------*
+
+#this is a comment for myself
+#they do not affect the program
