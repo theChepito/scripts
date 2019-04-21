793433cc8fc6b1e3f3c703d6ff01692a6cc71574
Joseph <joseph.p.schmitt@gmail.com>
2019-04-21 13:28:18 -0700

    Create script that creates a user defined script

---
+++ createScript.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# prompt user to define script name, assign to variable
+read -p "name of script to create: " name_s
+
+# create script using user defined name
+touch ${name_s}
+
+# append content to script file
+echo " #!/usr/bin/env bash" >> ${name_s}
+echo " ####### Script Created Automatically ####### " >> ${name_s}
+echo " pwd " >> ${name_s}
+echo " cal " >> ${name_s}
+
+# give file executable permissions
+chmod +x ${name_s}
+
+echo " DONE "
