9e102b9d9dcbcce385dc1a3a7c56a0c81226e2e6
Joseph <joseph.p.schmitt@gmail.com>
2019-04-24 20:47:35 -0700

    Create script to move backwards # in directory based on user input

---
+++ back.sh
@@ -0,0 +1,15 @@
+ #! /bin/bash
+ ####### Script Outline Created Automatically #######
+
+echo "This is the back script"
+
+max="$1";
+
+for (( i = 0; i -eq $max; i++ )); do
+  cd ../
+
+done
+
+
+
+echo "Present Directory: $(pwd)"
